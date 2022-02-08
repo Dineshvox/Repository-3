a374c2a4599218d6000cbaf36f961229b2a41384
Dineshvox <dineshmurugan23.07@gmail.com>
2022-02-08 09:15:45 +0530

    Added new line

--- src/org/java/Day1.java
+++ src/org/java/Day1.java
@@ -1,5 +1,11 @@
 package org.java;
 
-public class Day1 {
+public class Day1 { 
+	private void sysout() {
+		System.out.println("Task 1");
+		System.out.println("Task 2");
+		System.out.println("Task 3");
+
+	}
 
 }
