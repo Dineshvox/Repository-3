5ab1fb61f57ccd5221482545da9685be614f3953
Dineshvox <dineshmurugan23.07@gmail.com>
2022-02-08 10:10:51 +0530

    new

--- src/org/java/Day1.java
+++ src/org/java/Day1.java
@@ -2,8 +2,8 @@ package org.java;
 
 public class Day1 { 
 	private void sysout() {
-		System.out.println("Task 1");
 		System.out.println("Task 2");
+		System.out.println("Task 4");
 		System.out.println("Task 3");
 
 	}
